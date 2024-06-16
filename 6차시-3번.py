0cfc0ebbd265f31ce0ffd23a7ca163f6fc2da8ff
Irisong0818 <iris08186287@gmail.com>
2024-06-16 23:14:43 +0900

    수정

---
+++ 6차시-3번.py
@@ -0,0 +1,32 @@
+def get_student(student_list, key_value_list):
+    candidate=[]
+    for student in student_list:
+        match=True
+        for info in key_value_list:
+            if student[info[0]]!=info[1]:
+                match=False
+                break
+        if match:
+            candidate.append(student)
+    return candidate
+
+def edit_student(student_db, key_value_list, edit_list):
+    candidate_students=get_student(student_db, key_value_list)
+    print("CANDIDATE STUDENTS:", candidate_students)
+    for candidate in candidate_students:
+        for edit in edit_list:
+            candidate[edit[0]]=edit[1]
+    print("EDITED STUDENTS:", candidate_students)
+
+student_database=[
+    {'이름': '한넙죽', '학교': '한국중학교', '학년': 1, '연락처': '01012342345', '수업': ['Python', 'C']},
+    {'이름': '김거위,', '학교': '오리초등학교', '학년': 5, '연락처': '01023334444', '수업': ['Python']},
+    {'이름': '로라', '학교': '한국중학교', '학년': 2, '연락처': '01022354242', '수업': ['C++', 'Java']},
+    {'이름': '리사', '학교': '한국초등학교', '학년': 6, '연락처': '01022223333', '수업': ['C', 'C++']},
+    {'이름': '이철수', '학교': '대한초등학교', '학년': 5, '연락처': '01022448888', '수업': ['C', 'Python']},
+    {'이름': '남영희', '학교': '대한중학교', '학년': 1, '연락처': '01023223333', '수업': ['Python', 'Java']}
+]
+
+edit_student(student_database, [('이름', '로라'), ('학교', '한국중학교')], [('이름', '로나')])
+print()
+edit_student(student_database, [('학교', '한국중학교')], [('학교', '한국국제중학교')])
